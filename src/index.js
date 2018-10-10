00875eff822c884bf93e1b9af2739f69acaad910
JP Lynch <JPLynch35@gmail.com>
2018-10-10 10:05:27 -0600

    Add post fetch request

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
-})
+  getTopWord()
+});
+
+$(`#button`).click(sendWords($(`#text-area`)).innerHTML);
+
+const getTopWord = () => {
+  fetch(`https://wordwatch-api.herokuapp.com/api/v1/top_word`)
+    .then(response => response.json())
+    .then(topWord => displayWord(topWord))
+    .catch(error => console.error({error}))
+};
+
+const displayWord = (word) => {
+  $(`.word-count`).html(`${Object.keys(word.word)}: ${Object.values(word.word)}`)
+};
+
+const sendWords = (text) => {
+  debugger;
+};
+
+const seperateWords = (text) => {
+};
